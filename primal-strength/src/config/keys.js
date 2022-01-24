31ef897618cfc8c7b575fb885499dae05b63ebe4
martindwyer <martin@mdbytes.com>
2022-01-24 14:25:30 -0600

    first commit

---
+++ primal-strength/src/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  EMAILJS_USER: "user_HQixmTuG9mMXopsr97IYS",
+  WP_REST_GET_POSTS_URL: "https://gowildthings.com/wp-json/wp/v2/posts?_embed",
+  RECAPTCHA_SITE_KEY: "6LfzqyMeAAAAAJp-sL_mN7pKiSNIh3YPyxBY1MDX",
+};
