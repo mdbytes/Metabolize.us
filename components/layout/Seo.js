ff191abf4f557751354abc4f993ae96bae710172
martindwyer <martin@mdbytes.com>
2022-03-30 09:59:01 -0500

    Language config set to "en"

--- components/layout/Seo.js
+++ components/layout/Seo.js
@@ -14,6 +14,24 @@ const Seo = (props) => {
         name="keywords"
         content="powerlifting conditioning weight lifting fitness longevity"
       />
+      <meta property="og:title" content="Primal Strength and Conditioning" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://primal-strength.vercel.app/" />
+      <meta
+        property="og:image"
+        content="https://goprimalstrength.com/wp-content/uploads/2022/03/Kazam_screenshot_00023.png"
+      />
+
+      <meta name="twitter:title" content="Primal Strength and Conditioning" />
+      <meta
+        name="twitter:description"
+        content="Primal Strength Training and Conditioning, a website dedicated to strength, conditioning and longevity."
+      />
+      <meta
+        name="twitter:image"
+        content="https://goprimalstrength.com/wp-content/uploads/2022/03/Kazam_screenshot_00023.png"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
 };
