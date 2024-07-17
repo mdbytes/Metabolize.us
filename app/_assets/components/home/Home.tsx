f51eb1f8c26615cf163111e813f6b89d5065b678
Martin Dwyer <martin@mdbytes.com>
2024-07-17 15:52:14 -0500

    Beginning Metabolic Health

--- app/_assets/components/home/Home.tsx
+++ app/_assets/components/home/Home.tsx
@@ -11,61 +11,67 @@ import profilePic from '../../images/home/header_profile_pic-1.webp';
 import MartinsJourney from './MartinsJourney';
 
 const LandingPage = () => {
-  useEffect(() => {
-    require('../../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js');
-  }, []);
+    useEffect(() => {
+        require('../../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js');
+    }, []);
 
-  return (
-    <>
-      <div className="app">
-        <section id="home" className="intro-section">
-          <div className="container-fluid">
-            <div className="jumbotron row">
-              <div className="col-md-6 left">
-                <div className="site-title">
-                  <h1 className="headline text-center">Primal Strength</h1>
-                  <p className="tagline text-center">
-                    Living strong. Staying positive.
-                  </p>
-                </div>
-                <div className="btn-learn-more">
-                  <a className="btn btn-primary" href="#featured-posts">
-                    Read More{' '}
-                  </a>
-                </div>
-              </div>
-              <div className="col-md-6 right">
-                <div className="featured-image">
-                  <Image
-                    src={profilePic}
-                    alt="featured-image"
-                    className="img-fluid"
-                  />
-                </div>
-                <div className="site-description">
-                  <div className="heading text-center">
-                    <h3>Martin Dwyer</h3>
-                    <h6>
-                      CFT, SSC, OCC<sup>*</sup>
-                    </h6>
-                  </div>
-                  <p>
-                    Living strong and staying positive with strength training,
-                    nutrition and exercise.
-                  </p>
-                </div>
-              </div>
+    return (
+        <>
+            <div className="app">
+                <section id="home" className="intro-section">
+                    <div className="container-fluid">
+                        <div className="jumbotron row">
+                            <div className="col-md-6 left">
+                                <div className="site-title">
+                                    <h1 className="headline text-center">
+                                        Metabolic Strength
+                                    </h1>
+                                    <p className="tagline text-center">
+                                        Building metabolic strength.
+                                    </p>
+                                </div>
+                                <div className="btn-learn-more">
+                                    <a
+                                        className="btn btn-primary"
+                                        href="#featured-posts"
+                                    >
+                                        Read More{' '}
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="col-md-6 right">
+                                <div className="featured-image">
+                                    <Image
+                                        src={profilePic}
+                                        alt="featured-image"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                                <div className="site-description">
+                                    <div className="heading text-center">
+                                        <h3>Martin Dwyer</h3>
+                                        <h6>
+                                            CFT, SSC, OCC<sup>*</sup>
+                                        </h6>
+                                    </div>
+                                    <p>
+                                        Living strong and staying positive with
+                                        strength training, nutrition and
+                                        exercise.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
-          </div>
-        </section>
-      </div>
-      <FeaturedArticles />
-      <HomeSlider slides={sliderOne} />
-      <OurApproach />
-      <HomeSlider slides={sliderTwo} />
-      <MartinsJourney />
-    </>
-  );
+            <FeaturedArticles />
+            <HomeSlider slides={sliderOne} />
+            <OurApproach />
+            <HomeSlider slides={sliderTwo} />
+            <MartinsJourney />
+        </>
+    );
 };
 
 export default LandingPage;
