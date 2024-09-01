b14e0ba1230d141f3c26cec212bd4795d8680795
Martin Dwyer <martin@mdbytes.com>
2024-09-01 06:55:00 -0500

    Updates to home page

--- app/_assets/components/home/OurApproach.tsx
+++ app/_assets/components/home/OurApproach.tsx
@@ -24,104 +24,109 @@ export default function OurApproach() {
                                         height="75"
                                         className="img-fluid"
                                     />
-                                    Thanks for visiting this page. It was
-                                    created to share experience, insights and
-                                    lessons learned on my journey to wellness.
-                                    The material you will find here can be
-                                    described by a handful of{' '}
-                                    <Link href="/post/driving-forces">
-                                        driving force
-                                    </Link>{' '}
-                                    factors accumulated over the years. These
-                                    are based on my own research and life
-                                    experience. Maybe some of them look familiar
-                                    to you:
-                                </p>
-
-                                <ul
-                                    className="fa-ul"
-                                    style={{ width: '100%', clear: 'both' }}
-                                >
-                                    <li>
-                                        <span className="fa-li">
-                                            <i className="fa-solid fa-circle-arrow-right"></i>
-                                        </span>
-                                        The world is facing a three-fold
-                                        pandemic of diabetes, heart disease and
-                                        cancer.
-                                    </li>
-                                    <li>
-                                        <span className="fa-li">
-                                            <i className="fa-solid fa-circle-arrow-right"></i>
-                                        </span>
-                                        This pandemic is driven by obesity,
-                                        blood pressure, blood sugar and blood
-                                        lipid levels.
-                                    </li>
-                                    <li>
-                                        <span className="fa-li">
-                                            <i className="fa-solid fa-circle-arrow-right"></i>
-                                        </span>
-                                        Low-fat diets, diet centers, and diet
-                                        products have all failed for decades.
-                                    </li>{' '}
-                                    <li>
-                                        <span className="fa-li">
-                                            <i className="fa-solid fa-circle-arrow-right"></i>
-                                        </span>
-                                        Calorie counting diets do not work.
-                                    </li>
-                                    <li>
-                                        <span className="fa-li">
-                                            <i className="fa-solid fa-circle-arrow-right"></i>
-                                        </span>
-                                        We do not need more pills or shots.
-                                    </li>
-                                    <li>
-                                        <span className="fa-li">
-                                            <i className="fa-solid fa-circle-arrow-right"></i>
-                                        </span>
-                                        We need more whole foods and to
-                                        eliminate processed foods.
-                                    </li>
-                                    <li>
-                                        <span className="fa-li">
-                                            <i className="fa-solid fa-circle-arrow-right"></i>
-                                        </span>
-                                        We need more exercise.
-                                    </li>
-                                </ul>
-                                <p>
-                                    <Image
-                                        src={approachPic}
-                                        alt=""
-                                        height="800"
-                                        width="400"
-                                        className="transformation-photo"
-                                    />
                                     My name is Martin. I am over 60 years old
                                     and have been training most of my life. But
-                                    the challenges of obesity, high blood
-                                    pressure, and prediabetes were relentless.
+                                    serious health challenges with prediabetes,
+                                    lung disease and cancer were relentless.
                                     These challenges brought me to a point where
                                     I needed to change if I wanted to be around
                                     for my loved ones, including my
                                     grandchildren who are just getting started.
                                     <br />
                                     <br />
+                                    My health problems were rooted in my
+                                    metabolism, and the impact of today&apos;s
+                                    nutritional standards on my health. This
+                                    website was created to share experience,
+                                    insights and lessons learned on my battle
+                                    overcoming metabolic challenges.
+                                    <br />
+                                    <br />
+                                    After seven years of overcoming, I am more
+                                    committed than ever to the following
+                                    realities for today:{' '}
+                                </p>
+                                <div className="row">
+                                    <div className="col-md-8">
+                                        <ul
+                                            className="fa-ul"
+                                            style={{
+                                                width: '100%',
+                                                clear: 'both',
+                                            }}
+                                        >
+                                            <li>
+                                                <span className="fa-li">
+                                                    <i className="fa-solid fa-circle-arrow-right"></i>
+                                                </span>
+                                                The world is facing a three-fold
+                                                pandemic of diabetes, heart
+                                                disease and cancer.
+                                            </li>
+                                            <li>
+                                                <span className="fa-li">
+                                                    <i className="fa-solid fa-circle-arrow-right"></i>
+                                                </span>
+                                                This pandemic is driven by
+                                                processed foods and sedentary
+                                                lifestyles.
+                                            </li>
+                                            <li>
+                                                <span className="fa-li">
+                                                    <i className="fa-solid fa-circle-arrow-right"></i>
+                                                </span>
+                                                Low-fat diets, diet centers, and
+                                                diet products have all failed
+                                                for decades.
+                                            </li>{' '}
+                                            <li>
+                                                <span className="fa-li">
+                                                    <i className="fa-solid fa-circle-arrow-right"></i>
+                                                </span>
+                                                Calorie counting diets do not
+                                                work.
+                                            </li>
+                                            <li>
+                                                <span className="fa-li">
+                                                    <i className="fa-solid fa-circle-arrow-right"></i>
+                                                </span>
+                                                We do not need more pills or
+                                                shots.
+                                            </li>
+                                            <li>
+                                                <span className="fa-li">
+                                                    <i className="fa-solid fa-circle-arrow-right"></i>
+                                                </span>
+                                                We need to eat to be satisfied
+                                                and healthy.
+                                            </li>
+                                            <li>
+                                                <span className="fa-li">
+                                                    <i className="fa-solid fa-circle-arrow-right"></i>
+                                                </span>
+                                                We need more exercise.
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-md-4">
+                                        <Image
+                                            src={approachPic}
+                                            alt=""
+                                            height="800"
+                                            width="400"
+                                            className="transformation-photo"
+                                        />
+                                    </div>
+                                </div>
+
+                                <p>
                                     I set off on a journey to find a solution to
-                                    a lifetime of struggles. What I learned
-                                    changed my life. I no longer struggle with
-                                    obesity, high blood pressure or prediabetes,
-                                    live with more energy than I can remember,
-                                    and am stronger to handle whatever life
-                                    throws me in the future. This website is
+                                    a lifetime of health struggles. What I
+                                    learned changed my life. This website is
                                     more than just a place for research,
                                     information or encouragement, it is part of
-                                    my ongoing recovery.
-                                    <br />
-                                    <br />
-                                    On this website, you can read our{' '}
+                                    my ongoing recovery. On this website, you
+                                    can read our{' '}
                                     <Link href="#featured-posts">
                                         featured articles
                                     </Link>
@@ -139,14 +144,10 @@ export default function OurApproach() {
                         </div>
                     </div>
                     <div className="card col-lg-4 approach battle card">
-                        <h3 className="text-center">Metabolic Wellness</h3>
+                        <h3 className="text-center">Approach</h3>
 
                         <div className="table-wrapper">
                             <div className="metabolic-solution">
-                                <p className="text-center">
-                                    Meeting today&apos;s challenge with:
-                                </p>
-
                                 <ul className="fa-ul" style={{ width: '100%' }}>
                                     <li>
                                         <span className="fa-li">
